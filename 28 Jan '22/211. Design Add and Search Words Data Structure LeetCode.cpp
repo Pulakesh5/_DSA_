52ac9df16a0aaed2559bdde3dd1d4eaf94c7ec75
Pulakesh5 <68358402+Pulakesh5@users.noreply.github.com>
2022-01-28 20:46:21 +0530

    Create 211. Design Add and Search Words Data Structure LeetCode.cpp

---
+++ 28 Jan '22/211. Design Add and Search Words Data Structure LeetCode.cpp
@@ -0,0 +1,59 @@
+// Same question with Trie implementation with slight modifications in the find function 
+
+class TrieNode{
+public:
+    TrieNode *next[26];
+    bool isword;
+    TrieNode(bool flag=false){
+        memset(next,0,sizeof(next));
+        isword=flag;
+    }
+};
+class WordDictionary {
+public:
+    TrieNode *root;
+    WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    void addWord(string word) {
+        TrieNode *p=root;
+        for(int i=0;i<word.length();i++){
+            if(p->next[word[i]-'a']==NULL)
+                p->next[word[i]-'a'] = new TrieNode();
+            p = p->next[word[i]-'a'];
+        }
+        p->isword=true;
+    }
+    
+    bool search(string word) {
+        return bfs(word, 0, root);
+    }
+private:
+    bool bfs(string word, int pos, TrieNode* node){
+        
+        for(int i=pos;i<word.length() && node; ++i){
+            if(word[i]!='.'){
+                node = node->next[word[i]-'a'];
+            }
+            else{
+                TrieNode* tmp = node;
+                for (int j = 0; j < 26; j++) {
+                    node = tmp ->next[j];
+                    if (bfs(word, i + 1, node)) {
+                        return true;
+                    }
+                }
+            }
+        }
+         return node && node ->isword;
+        
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
