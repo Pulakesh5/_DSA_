dc234d6e6f000a7200d48c3e5524ad5f8ef58fa9
Pulakesh5 <68358402+Pulakesh5@users.noreply.github.com>
2022-02-25 22:38:50 +0530

    Create 165. Compare Version Numbers leetcode.cpp

---
+++ 25 Feb '22/165. Compare Version Numbers leetcode.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int i = 0, j = 0, n1 = version1.size(), n2 = version2.size(), num1 = 0, num2 = 0; 
+        while(i<n1 || j<n2)
+        {
+            while(i<n1 && version1[i]!='.') // computing groups of digits from version 1
+            {
+                num1 = num1*10+(version1[i]-'0');
+                i++;
+            }
+            while(j<n2 && version2[j]!='.') // computing groups of digits from version 2
+            {
+                num2 = num2*10+(version2[j]-'0');
+                j++;
+            }
+            // comparings the numbers 
+            if(num1>num2)
+            {
+                return 1;
+            }
+            if(num1 < num2) 
+            {
+                return -1;
+            }
+            i++;
+            j++;
+            num1 = 0;
+            num2 = 0;
+        }
+        return 0; 
+    }
+};
