3b2be3b9f71fd6a6acb13fade9f9c724298b1523
Pulakesh5 <68358402+Pulakesh5@users.noreply.github.com>
2022-03-16 22:22:09 +0530

    Update Subsets gfg.cpp

--- March 16 '22/Subsets gfg.cpp
+++ March 16 '22/Subsets gfg.cpp
@@ -1,3 +1,4 @@
+//BitSet Approach
 class Solution
 {
     public:
@@ -17,4 +18,33 @@ class Solution
         sort(ans.begin(),ans.end());
         return ans;
     }
+};
+
+//BackTracking Approach
+class Solution
+{
+   vector<vector<int>> subsetsRes; //global vector declaration
+
+   //recursive helper function
+   void generate(vector<int>& A, int i, vector<int>& nums){
+   
+       if(i==A.size()){
+           subsetsRes.push_back(nums);
+           return;
+       }    // base case 
+       nums.push_back(A[i]);
+      generate(A, i+1, nums); // Case in which element considered is  added
+      nums.pop_back();
+      generate(A, i+1, nums); // Case in which element considered is not added
+       //backtrack
+   }
+   public:
+   vector<vector<int>> subsets(vector<int>& A)
+   {
+       //code here
+       vector<int> nums;
+       generate(A, 0, nums);
+       sort(subsetsRes.begin(), subsetsRes.end());
+       return subsetsRes;
+   }
 };
