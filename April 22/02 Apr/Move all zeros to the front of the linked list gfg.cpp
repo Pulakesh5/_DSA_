633a5dcf7f938b2992e7c40860f706fd596e4388
Pulakesh5 <68358402+Pulakesh5@users.noreply.github.com>
2022-04-02 22:05:36 +0530

    Create Move all zeros to the front of the linked list gfg.cpp

---
+++ April 22/02 Apr/Move all zeros to the front of the linked list gfg.cpp
@@ -0,0 +1,96 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+void push(struct Node **head_ref, int new_data) {
+    struct Node *new_node = new Node(new_data);
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+void moveZeroes(struct Node **head);
+
+// function to displaying nodes
+void display(struct Node *head) {
+    while (head != NULL) {
+	    cout << head->data << " ";
+	    head = head->next;
+    }
+    cout <<endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int q;
+        struct Node *Head=NULL;
+        for(int i=0;i<n;i++)
+        {
+            cin>>q;
+            push(&Head,q);
+        }
+        moveZeroes(&Head);
+        display(Head);
+
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for C++
+/* Linked List Node structure
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+*/
+// Move Zeros to the front of the list
+void moveZeroes(struct Node **head)
+{
+    Node *start= *head;
+    
+    vector<int> arr;
+    int zero=0;
+    while(start){
+        if(start->data){
+            arr.push_back(start->data);   
+        }
+        else
+        zero++;
+        start=start->next;
+    }
+    start=*head;
+    while(zero--){
+       start->data=0;
+       start=start->next;
+    }
+    int size=arr.size(),i=0;
+    while(size--){
+        start->data=arr[i++];
+        start=start->next;
+    }
+}
