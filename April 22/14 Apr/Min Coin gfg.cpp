c04babaa9fda1e2bc7146435842d1d867fd5732f
Pulakesh5 <68358402+Pulakesh5@users.noreply.github.com>
2022-04-14 00:49:32 +0530

    Create Min Coin gfg.cpp

---
+++ April 22/14 Apr/Min Coin gfg.cpp
@@ -0,0 +1,19 @@
+class Solution{
+	public:
+	int MinCoin(vector<int>coins, int amount)
+	{      
+      int n = nums.size();
+	    vector<int> dp(amount+1, INT_MAX);
+	    dp[0] = 0;
+	    for(int i=1;i<=amount;i++)
+	    {
+	        for(int j=0;j<n;j++)
+	        {
+	            if(nums[j] <= i && dp[i-nums[j]] != INT_MAX) 
+	                dp[i] = min(dp[i], 1 + dp[i-nums[j]]);
+	        }
+	    }
+	    if(dp[amount] == INT_MAX) return -1;
+	    else return dp[amount];
+	}
+};
