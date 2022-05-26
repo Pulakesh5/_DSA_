0be883d54b638456ad9f05a75c05c93171ade564
Pulakesh5 <68358402+Pulakesh5@users.noreply.github.com>
2022-05-26 21:46:17 +0530

    Update 2. Find duplicates in an array gfg.cpp

--- May 22/26 May/2. Find duplicates in an array gfg.cpp
+++ May 22/26 May/2. Find duplicates in an array gfg.cpp
@@ -13,3 +13,22 @@ class Solution{
         return ans;
     }
 };
+
+//Much better solution
+class Solution{
+  public:
+    vector<int> duplicates(int arr[], int n) {
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            int index=arr[i]%n;
+            arr[index]+=n;
+        }
+        for(int i=0;i<n;i++){
+            if((arr[i]/n)>=2){
+                ans.push_back(i);
+            }   
+        }
+        if(ans.size()==0) return {-1};
+        return ans;
+    }
+};
