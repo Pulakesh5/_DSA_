eef8c5d07579bde83f1a8397517e69c1c00c684d
Pulakesh5 <68358402+Pulakesh5@users.noreply.github.com>
2022-06-28 16:19:13 +0530

    Create 1647. Minimum Deletions to Make Character Frequencies Unique LC.cpp

---
+++ June 22/28 June/1647. Minimum Deletions to Make Character Frequencies Unique LC.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        vector<int> count(26,0);
+        map<int,int> mp;
+        priority_queue<int> pq;
+        int n=s.size();
+        for(int i=0;i<n;i++)
+            count[s[i]-'a']++;
+        for(int i=0;i<26;i++){
+            if(count[i]>0)
+            {
+                mp[count[i]]++;
+                pq.push(count[i]);
+            }
+        }
+        int ans=0;
+        while(pq.size()>1){
+            int tp1=pq.top();
+            pq.pop();
+            int tp2=pq.top();
+            if(tp1==tp2){
+                while(tp1>0 && mp.find(tp1)!=mp.end()){
+                    ans++;tp1--;
+                }
+                mp[tp2]--;
+                mp[tp1]++;
+                if(tp1>0)
+                    pq.push(tp1);
+            }
+        }
+        
+        return ans;
+    }
+};
