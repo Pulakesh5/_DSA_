9c6ee70ccb3cf0882af00734332134de76997965
Pulakesh5 <68358402+Pulakesh5@users.noreply.github.com>
2022-07-04 22:43:18 +0530

    Create 135. Candy LC.cpp

---
+++ July 22/04 July/135. Candy LC.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        vector<int> ans(n,1);
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1])
+                ans[i]=ans[i-1]+1;
+        }
+        for(int i=n-1;i>0 ;i--){
+            if(ratings[i-1]>ratings[i])
+                ans[i-1]=max(ans[i-1],ans[i]+1);
+        }
+        int answer=0;
+        
+        for(int i=0;i<n;i++)
+            answer+=ans[i];
+        
+        return answer;
+    }
+};
