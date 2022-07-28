405019e576129526f83517e71c1cf9d85d3bbbe7
Pulakesh5 <68358402+Pulakesh5@users.noreply.github.com>
2022-07-28 17:24:27 +0530

    Create 62. Unique Paths LC.cpp

---
+++ July 22/25 July/62. Unique Paths LC.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        // combination of m-1 D and n-1 R moves
+       if(m==1 || n==1) return 1;
+        m--; 
+        n--;
+        if(m<n){
+            swap(m,n);
+        }
+        long long j=1,res=1;
+        for(int i=m+1;i<=(m+n);i++,j++){
+            res*=i;
+            res/=j;
+        }
+        return (int) res;
+    }
+    
+    
+};
