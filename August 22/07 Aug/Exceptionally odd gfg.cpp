b8d9f1f8a99313f7a82f215dac2da11c1801c544
Pulakesh5 <68358402+Pulakesh5@users.noreply.github.com>
2022-08-13 19:34:23 +0530

    Create Exceptionally odd gfg.cpp

---
+++ August 22/07 Aug/Exceptionally odd gfg.cpp
@@ -0,0 +1,12 @@
+class Solution{   
+public:
+    int getOddOccurrence(int arr[], int n) {
+        // code here
+        int res=0;
+        for(int i=0;i<n;i++)
+        {
+            res ^=arr[i];
+        }
+        return res;
+    }
+};
