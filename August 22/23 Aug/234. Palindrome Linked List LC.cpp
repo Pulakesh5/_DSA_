f759805600ee3b04b8327f41083cc69b5ba823ac
Pulakesh5 <68358402+Pulakesh5@users.noreply.github.com>
2022-08-23 19:44:57 +0530

    Create 234. Palindrome Linked List LC.cpp

---
+++ August 22/23 Aug/234. Palindrome Linked List LC.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* root=head;
+        string s="";
+        while(root){
+            s+=(root->val+'0');
+            root=root->next;
+        }
+        int l=0,r=s.size()-1;
+        while(l<r){
+            if(s[l]!=s[r])
+                return false;
+            l++;r--;
+        }
+        return true;
+    }
+};
