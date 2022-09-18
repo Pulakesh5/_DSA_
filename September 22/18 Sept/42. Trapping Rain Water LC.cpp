c68722f4c6de6f7548f873750dc6757147f4b25b
Pulakesh5 <68358402+Pulakesh5@users.noreply.github.com>
2022-09-18 17:10:18 +0530

    Create 42. Trapping Rain Water LC.cpp

---
+++ September 22/18 Sept/42. Trapping Rain Water LC.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        
+        vector<int> left(n),right(n);
+        left[0]=height[0];
+        right[n-1]=height[n-1];
+        
+        for(int i=1;i<n;i++)
+        {
+            left[i]=max(height[i],left[i-1]);
+            
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            right[i]=max(height[i],right[i+1]);
+        }
+        
+        int mxheight,trappedWater=0;
+        for(int i=0;i<n;i++)
+        {
+            mxheight=min(right[i],left[i]);
+            trappedWater+=max(0,mxheight-height[i]);
+        }
+        return trappedWater;
+    }
+};
