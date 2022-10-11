82f9d66310fc0b89dfd8a4afd410c8505c0da6a7
Pulakesh5 <68358402+Pulakesh5@users.noreply.github.com>
2022-10-11 22:28:15 +0530

    Create Decode the string gfg.cpp

---
+++ October 22/11 Oct/Decode the string gfg.cpp
@@ -0,0 +1,30 @@
+    string decodedString(string s){
+        // code here
+        int count=0;
+        string sub="";
+        while(s[i]>='0' && s[i]<='9')
+        {
+            count=(count*10+s[i]-'0');
+            i++;
+        }
+        i++; // for [
+        while(s[i]!=']')
+        {
+            if(s[i]>='a' && s[i]<='z')
+            {
+                sub+=s[i];
+                i++;
+            }
+            else
+                sub+=decodedString(s);
+        }
+        i++;
+        string temp="";
+        while(count--)
+            temp+=sub;
+        
+        while(i<s.size() && s[i]>='0' && s[i]<='9') temp+=decodedString(s);
+        
+        return temp;
+        
+    }
