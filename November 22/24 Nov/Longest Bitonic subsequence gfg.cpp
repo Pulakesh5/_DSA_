b39366564da380d5403c6a62a52aa401bc096ef5
Pulakesh5 <68358402+Pulakesh5@users.noreply.github.com>
2022-11-24 20:51:46 +0530

    Create Longest Bitonic subsequence gfg.cpp

---
+++ November 22/24 Nov/Longest Bitonic subsequence gfg.cpp
@@ -0,0 +1,24 @@
+	int LongestBitonicSequence(vector<int>nums)
+	{
+	    // code here
+	    int n=nums.size();
+	    vector<int> lis(n,1);
+	    vector<int> lds(n,1);
+	    
+	    for(int i=1;i<n;i++)
+	        for(int j=0;j<i;j++)
+	            if(nums[i]>nums[j] && lis[i] < lis[j]+1)
+	                lis[i] = lis[j]+1;
+	                
+	    for(int i=n-2;i>=0;i--)
+	        for(int j=n-1;j>i;j--)
+	            if(nums[i]>nums[j] && lds[i] < lds[j]+1)
+	                lds[i] = lds[j]+1;
+	                
+	    int ans=lis[0]+lds[0]-1;
+	   
+	    for(int i=0;i<n;i++)
+	        ans=max((lis[i]+lds[i]-1 ), ans);
+	    
+	    return ans;
+	}
