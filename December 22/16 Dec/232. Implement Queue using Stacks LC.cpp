dff402b1e675a11d387db59b29c4d9b7816833f8
Pulakesh5 <68358402+Pulakesh5@users.noreply.github.com>
2022-12-16 18:21:37 +0530

    Create 232. Implement Queue using Stacks LC.cpp

---
+++ December 22/16 Dec/232. Implement Queue using Stacks LC.cpp
@@ -0,0 +1,47 @@
+class MyQueue {
+public:
+    stack<int> st1,st2;
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        st1.push(x);
+    }
+    
+    int pop() {
+        while(!st1.empty())
+        {
+            st2.push(st1.top());
+            st1.pop();
+        }
+        int ans=st2.top();
+        st2.pop();
+        while(!st2.empty())
+        {
+            st1.push(st2.top());
+            st2.pop();
+        }
+        return ans;
+    }
+    
+    int peek() {
+        while(!st1.empty())
+        {
+            st2.push(st1.top());
+            st1.pop();
+        }
+        int ans=st2.top();
+        //st2.pop();
+        while(!st2.empty())
+        {
+            st1.push(st2.top());
+            st2.pop();
+        }
+        return ans;
+    }
+    
+    bool empty() {
+        return st1.empty();
+    }
+};
