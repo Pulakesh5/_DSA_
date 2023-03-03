72a4bb560111e053f7a111006979c68984f6f47d
Pulakesh Bag <68358402+Pulakesh5@users.noreply.github.com>
2023-03-03 21:25:49 +0530

    Create Cutting Rectangles gfg.cpp

---
+++ March 23/03 Mar/Cutting Rectangles gfg.cpp
@@ -0,0 +1,6 @@
+    vector<long long int> minimumSquares(long long int L, long long int B)
+    {
+        long long int K=__gcd(L,B);
+        long long int N=L/K * B/K;
+        return {N,K};
+    }
