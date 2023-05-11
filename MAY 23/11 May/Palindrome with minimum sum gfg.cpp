84a1b8802e5594a9f23fe3c03dfca6134b9c1dfd
Pulakesh Bag <68358402+Pulakesh5@users.noreply.github.com>
2023-05-11 23:21:14 +0530

    Create Palindrome with minimum sum gfg.cpp

---
+++ MAY 23/11 May/Palindrome with minimum sum gfg.cpp
@@ -0,0 +1,25 @@
+    int minimumSum(string s) {
+        int n = s.size();
+        int i,j;
+        if(n%2 == 0){
+            i = n/2-1;  j = n/2;
+        }else{
+            i = n/2;  j = n/2;
+        }
+        char prev = '#';
+        int ans=0;
+        while(i >= 0){
+            if(s[i] != '?' && s[n-i-1] != '?' && s[i] != s[n-i-1]) return -1;
+            else if(s[i] == '?' && s[j] == '?'){i--;j++; continue;}
+            else if(s[i] != '?' && s[i] != prev){
+                if(prev != '#') ans += 2*abs(s[i]-prev);
+                prev = s[i];
+            }
+            else if(s[j] != '?' && s[j] != prev){
+                if(prev != '#') ans += 2*abs(s[j]-prev);
+                prev = s[j];
+            }
+            i--; j++;
+        }
+        return ans;
+    }
