0eabdac5b6b6846ab1063e48777eb5ab3210c48f
Pulakesh Bag <68358402+Pulakesh5@users.noreply.github.com>
2023-05-21 21:04:45 +0530

    Create Bus Conductor gfg.cpp

---
+++ MAY 23/21 May/Bus Conductor gfg.cpp
@@ -0,0 +1,11 @@
+    int findMoves(int n, vector<int> chairs, vector<int> passengers) {
+        sort(chairs.begin(), chairs.end());
+        sort(passengers.begin(), passengers.end());
+        
+        int moves=0;
+        for(int i=0; i<n; i++)
+        {
+            moves+=abs(chairs[i] - passengers[i]);
+        }
+        return moves;
+    }
