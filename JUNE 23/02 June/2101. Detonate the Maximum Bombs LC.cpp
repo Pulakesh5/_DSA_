c54d5d68766c81171075fce9b37add14b9b31a26
Pulakesh Bag <68358402+Pulakesh5@users.noreply.github.com>
2023-06-03 01:10:28 +0530

    Create 2101. Detonate the Maximum Bombs LC.cpp

---
+++ JUNE 23/02 June/2101. Detonate the Maximum Bombs LC.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>> &gr,vector<bool> &visited,int &c,int &i) {
+        visited[i]=true;
+        c++;
+        for(int j=0;j<gr[i].size();j++) {
+            if(!visited[gr[i][j]])
+                dfs(gr,visited,c,gr[i][j]);   
+        }
+    }
+    int maximumDetonation(vector<vector<int>>& bombs) {
+        int n = bombs.size();
+        vector<vector<int>> next(n);
+        long long x1, y1, r1;
+        for(int i=0; i<n; i++)
+        {
+            x1 = bombs[i][0];
+            y1 = bombs[i][1];
+            r1 = bombs[i][2];
+            for(int j=0; j<n; j++)
+            {
+                if(i == j) continue;
+                long long x = abs(x1 - bombs[j][0]);
+                long long y = abs(y1 - bombs[j][1]);
+                if(x*x + y*y <= r1*r1)
+                    next[i].push_back(j);
+            }
+        }
+        int ans=INT_MIN;
+        for(int i=0 ;i<n; i++)
+        {
+            int c=0;
+            vector<bool> visited(n,false);
+            dfs(next,visited,c,i);
+            ans=max(ans,c);
+        }
+        return ans;
+    }
+};
