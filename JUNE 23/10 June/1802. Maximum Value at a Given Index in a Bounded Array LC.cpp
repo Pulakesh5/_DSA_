57356897da1fb2859a0084262b36092fc9f6722e
Pulakesh Bag <68358402+Pulakesh5@users.noreply.github.com>
2023-06-10 22:41:31 +0530

    Update 1802. Maximum Value at a Given Index in a Bounded Array LC.cpp

--- JUNE 23/10 June/1802. Maximum Value at a Given Index in a Bounded Array LC.cpp
+++ JUNE 23/10 June/1802. Maximum Value at a Given Index in a Bounded Array LC.cpp
@@ -1,22 +1,51 @@
-class Solution
-{
-    public:
-        void Rearrange(int arr[], int n)
+class Solution {
+public:
+    bool check(long long mid, long long ind, long long maxSum, int n)
+    {
+        long long ans = sum(mid-1, ind) + sum(mid, n - ind);
+        return ans <= maxSum;
+    }
+    long long sum(long long a, int n)
+    {
+        long  an = 0, extras = 0;
+        long long ans = 0;
+        if(n>=a)
         {
-            // Your code goes here
-            int neg = 0;
-            vector<int> v;
-            for(int i=0; i<n; i++)
-            {
-                if(arr[i]<0)
-                {
-                    v.insert(v.begin()+neg,arr[i]);
-                    neg++;                    
-                }
-                else
-                    v.push_back(arr[i]);
-            }
-            for(int i=0; i<n; i++)
-                arr[i] = v[i];
+            an = 1;
+            extras = (n-a);
         }
+        else if(n<a)
+        {
+            extras = 0;
+            an = a - n + 1;
+        }
+        ans = ((an+a) * (a-an+1))/2;
+        ans += extras;
+        return ans;
+    }
+    int maxValue(int n, int index, int maxSum) {
+        if(n==1) return maxSum;
+        if(n==2) return (maxSum+1)/2;
+        long long low = 1, high = maxSum, mid;
+        int max_at_index = 1;
+        
+        while(low<=high)
+        {
+            mid = (low+high)/2;
+            // cout<<mid<<" : ";
+            if(check(mid, index, maxSum, n))
+            {
+                max_at_index = mid;
+                // cout<<"returned true\n";
+                low = mid + 1;
+            }
+            else
+            {
+                // cout<<"returned false\n";
+                high = mid - 1;
+            }
+        }
+        return max_at_index;
+    }
 };
+
