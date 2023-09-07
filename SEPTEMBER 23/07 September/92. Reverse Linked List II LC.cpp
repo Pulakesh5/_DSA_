ee73b4cb99aefdad54875429714ab65ed177f71d
Pulakesh Bag <pulakesh.bag.1729@gmail.com>
2023-09-07 19:06:52 +0530

    Create 92. Reverse Linked List II LC.cpp

---
+++ SEPTEMBER 23/07 September/92. Reverse Linked List II LC.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if(!head) return head;
+        ListNode *root=head;
+        int n=0;
+        vector<int> v;
+        while(root){
+            n++;
+            //if(n>=left && n<=right) 
+            v.push_back(root->val);
+            root=root->next;
+        }
+        reverse(v.begin()+left-1,v.begin()+right);
+        root=head;
+        for(int i=1;i<=n;i++){
+            if(i>=left && i<=right){
+                root->val=v[i-1];
+                v.pop_back();
+            }
+            root=root->next;
+        }
+        return head;
+    }
+};
