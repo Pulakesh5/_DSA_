6ef30709d633b047c7ad303fee8a6b4287c70b3d
Pulakesh Bag <pulakesh.bag.1729@gmail.com>
2023-10-04 22:34:51 +0530

    Create 706. Design HashMap LC.cpp

---
+++ OCTOBER 23/04 October/706. Design HashMap LC.cpp
@@ -0,0 +1,28 @@
+class MyHashMap {
+public:
+    int MAX = 1e6+5;
+    vector<int> hashMap; 
+    MyHashMap() {
+        hashMap.resize(MAX, -1);
+    }
+    
+    void put(int key, int value) {
+        hashMap[key] = value;
+    }
+    
+    int get(int key) {
+        return hashMap[key];
+    }
+    
+    void remove(int key) {
+        hashMap[key] = -1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
