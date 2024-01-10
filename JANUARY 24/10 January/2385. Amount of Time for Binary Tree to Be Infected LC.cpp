b1a85ef0ab4c889a7a101aef421d95d8b04ad805
Pulakesh Bag <pulakesh.bag.1729@gmail.com>
2024-01-11 00:11:07 +0530

    Create 2385. Amount of Time for Binary Tree to Be Infected LC.cpp

---
+++ JANUARY 24/10 January/2385. Amount of Time for Binary Tree to Be Infected LC.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    //res to store maximum distance from the first infected node
+    int res = 0;
+    pair<bool,int> dfs(TreeNode* root, int start){
+        if(!root) return {false,0};
+
+        pair<bool,int>p1 = dfs(root->left,start);
+        pair<bool,int>p2 = dfs(root->right,start);
+
+        if(root->val==start){
+            int temp = max(p2.second,p1.second);
+            res = max(res,temp);
+            return {true,0};
+        }
+
+        if(p1.first){
+            int sum = p1.second+p2.second+1;
+            res = max(res,sum);
+            return {true,p1.second+1};
+        }else if(p2.first){
+            int sum = p1.second+p2.second+1;
+            res = max(res,sum);
+            return {true,p2.second+1};
+        }
+
+        int sum = max(p1.second,p2.second);
+        return {false,1+sum};
+    }
+    
+    int amountOfTime(TreeNode* root, int start) {
+        dfs(root,start);
+        return res;
+    }
+};
