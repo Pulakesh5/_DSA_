3caef5ecaa3fc731d696674632718e13f1a7d939
Pulakesh Bag <pulakesh.bag.1729@gmail.com>
2024-02-20 23:35:16 +0530

    Create Word Break gfg.cpp

---
+++ FEBRUARY 24/20 February/Word Break gfg.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    int wordBreak(int n, string s, vector<string> &dictionary) {
+        //code here
+        int ns = s.size();
+        
+        vector<bool> dp(ns+1, 0);
+        dp[0] = 1;
+        
+        for(int i = 0; i<ns; i++){
+            if(!dp[i]) continue;
+            
+            string ss;
+            for(int j = i; j<ns; j++){
+                ss.push_back(s[j]);
+                for(auto _: dictionary){
+                    if(ss==_) {dp[j+1] = 1; break;}
+                }
+            }
+        }
+        
+        return dp[ns];
+    }
+};
