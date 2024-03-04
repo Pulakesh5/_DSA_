9208d40b8dec7eb9f101dfda28b266a45d5de71f
Pulakesh Bag <pulakesh.bag.1729@gmail.com>
2024-03-04 22:54:45 +0530

    Create Swap the array elements gfg.cpp

---
+++ MARCH 24/04 March/Swap the array elements gfg.cpp
@@ -0,0 +1,10 @@
+class Solution{
+  public:
+    void swapElements(int arr[], int n){
+        
+        // Your code here
+        for(int i=0; i<(n-2); i++)
+            swap(arr[i], arr[i+2]);
+        
+    }
+};
