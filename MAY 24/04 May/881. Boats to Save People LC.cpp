e0bde9447595cdf1070b8a82056094c840842437
Pulakesh Bag <pulakesh.bag.1729@gmail.com>
2024-05-04 16:13:27 +0530

    Create 881. Boats to Save People LC.cpp

---
+++ MAY 24/04 May/881. Boats to Save People LC.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int l = 0, r = people.size()-1;
+        int boat = 0;
+        while(l<=r)
+        {
+            if(people[l]+people[r]<=limit)
+            {
+                l++;
+                r--;
+            }
+            else
+                r--;
+            boat++;
+        }
+        return boat;
+    }
+};
