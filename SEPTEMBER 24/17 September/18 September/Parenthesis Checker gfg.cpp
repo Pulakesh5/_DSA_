25f93736c6010b5bcdcee17a4f78c475c2853a4b
Pulakesh Bag <pulakesh.bag.1729@gmail.com>
2024-09-18 23:41:16 +0530

    Create Parenthesis Checker gfg.cpp

---
+++ SEPTEMBER 24/17 September/18 September/Parenthesis Checker gfg.cpp
@@ -0,0 +1,40 @@
+class Solution
+{
+    public:
+    //Function to check if brackets are balanced or not.
+    bool ispar(string x)
+    {
+        // Your code here
+        stack<char> stk;
+        for(char ch:x)
+        {
+            if(ch=='(' || ch=='{' || ch=='[')
+                stk.push(ch);
+            else if(ch == ')')
+            {
+                if(!stk.empty() && stk.top() == '(')
+                    stk.pop();
+                else
+                    return false;
+            }
+            else if(ch == '}')
+            {
+                if(!stk.empty() && stk.top() == '{')
+                    stk.pop();
+                else
+                    return false;
+            }
+            else if(ch == ']')
+            {
+                if(!stk.empty() && stk.top() == '[')
+                    stk.pop();
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+        return stk.empty();
+    }
+
+};
