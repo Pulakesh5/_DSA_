99b90f6e46901cf8df1babbc9e31ba46ece6bf21
Pulakesh Bag <pulakesh.bag.1729@gmail.com>
2024-10-27 20:32:53 +0530

    Update 1277. Count Square Submatrices with All Ones LC.cpp

--- OCTOBER 24/27 October/1277. Count Square Submatrices with All Ones LC.cpp
+++ OCTOBER 24/27 October/1277. Count Square Submatrices with All Ones LC.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     int countSquares(vector<vector<int>>& matrix) {
-        int count = 0;
-        int m = matrix.size(), n = matrix[0].size();
-        for(int i=0; i<m; i++)
-        {
-            for(int j=0; j<n; j++)
-            {
-                if(matrix[i][j]==1)
-                {
-                    if(i>0 && j>0)
-                        matrix[i][j] += min({matrix[i-1][j], matrix[i][j-1], matrix[i-1][j-1]});
-                    count += matrix[i][j];
+        int n = matrix.size(), m = matrix[0].size();
+        vector<vector<int>> dp(n, vector<int>(m,0));
+        int ans = 0;
+        for(int i=0; i<n; i++) {
+            dp[i][0] = matrix[i][0];
+            ans += dp[i][0];
+        }
+        for(int j=1; j<m; j++) {
+            dp[0][j] = matrix[0][j];
+            ans += dp[0][j];
+        }
+
+        for(int i=1; i<n; i++) {
+            for(int j=1; j<m; j++) {
+                if(matrix[i][j]==1) {
+                    dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
                 }
+                ans += dp[i][j];
             }
         }
-        return count;
+        return ans;
     }
 };
