452add9cfe2edf3113b8a30970059617a1c5fa95
Pulakesh Bag <pulakesh.bag.1729@gmail.com>
2024-11-14 23:27:32 +0530

    Create Nearly sorted gfg.cpp

---
+++ NOVEMBER 24/Nearly sorted gfg.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    void nearlySorted(vector<int>& arr, int k) {
+        // code
+        priority_queue<int, vector<int>, greater<int>> pq(arr.begin(), arr.begin() + k + 1);
+        int j = 0;
+        for (int i = k + 1; i < arr.size(); i++)
+        {
+            arr[j++] = pq.top();
+            pq.pop();
+            pq.push(arr[i]);
+        }
+        while (!pq.empty())
+        {
+            arr[j++] = pq.top();
+            pq.pop();
+        }
+    }
+};
