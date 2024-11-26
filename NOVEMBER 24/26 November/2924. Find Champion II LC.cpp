80f00dc355342cdf092af49c2e0d7828c000d68c
Pulakesh Bag <pulakesh.bag.1729@gmail.com>
2024-11-26 20:51:17 +0530

    Create 2924. Find Champion II LC.cpp

---
+++ NOVEMBER 24/26 November/2924. Find Champion II LC.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findChampion(int n, vector<vector<int>>& edges) {
+        vector<int> indegree(n);
+        for(auto &edge:edges)
+            indegree[edge[1]]++;
+
+        int winner = -1;
+        for(int i=0; i<n; i++)
+        {
+            if(indegree[i] == 0)
+            {
+                if(winner==-1)
+                    winner = i;
+                else
+                    return -1;
+            }
+        }
+        return winner;
+    }
+};
