162d7df586677b992311be4a4b4029029d871dbe
Pulakesh Bag <pulakesh.bag.1729@gmail.com>
2025-01-25 21:05:21 +0530

    Create Find the first node of loop in linked list gfg.cpp

---
+++ JANUARY 2025/25 January/Find the first node of loop in linked list gfg.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    Node* findFirstNode(Node* head) {
+        // your code here
+        Node *slow = head, *fast = head;
+        while(fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if(slow==fast)
+            {
+                slow = head;
+                while(slow != fast)
+                {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return fast;
+            }
+        }
+        return nullptr;
+    }
+};
