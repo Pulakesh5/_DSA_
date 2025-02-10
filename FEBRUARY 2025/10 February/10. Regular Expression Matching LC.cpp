1a94db178054ac5e87c6639a42d552438e7f2913
Pulakesh Bag <pulakesh.bag.1729@gmail.com>
2025-02-10 23:37:41 +0530

    Create 10. Regular Expression Matching LC.cpp

---
+++ FEBRUARY 2025/10 February/10. Regular Expression Matching LC.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.size(), n = p.size();
+        vector<bool> pre(n + 1, false), cur(n + 1, false);
+        cur[0] = true;
+        for (int i = 0; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p[j - 1] == '*') {
+                    cur[j] = cur[j - 2] || (i && pre[j] && (s[i - 1] == p[j - 2] || p[j - 2] == '.'));
+                } else {
+                    cur[j] = i && pre[j - 1] && (s[i - 1] == p[j - 1] || p[j - 1] == '.');
+                }
+            }
+            fill(pre.begin(), pre.end(), false);
+			swap(pre, cur);
+        }
+        return pre[n];
+    }
+};
