475b2161be25cda84e9e42bbbffd03c03502a7b7
Pulakesh Bag <pulakesh.bag.1729@gmail.com>
2025-07-08 01:32:43 +0530

    Create Next Greater Element in Circular Array gfg.java

---
+++ JULY 2025/07 July/Next Greater Element in Circular Array gfg.java
@@ -0,0 +1,41 @@
+class Solution {
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+        // code here
+         int n = arr.length;
+        
+        int max = -1, ind = -1;
+        for(int i =0; i < n; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+                ind = i;
+            }
+        }
+        
+        Stack<Integer> stk = new Stack<>();
+        stk.push(max);
+        ArrayList<Integer> ll = new ArrayList<>(Collections.nCopies(n, -1));
+        ll.set(ind, -1);
+        int i = ind -1;
+        while(i >= 0) {
+            while(!stk.isEmpty() && stk.peek() <= arr[i]) stk.pop();
+            int temp = arr[i];
+            if(!stk.isEmpty()) {
+                ll.set(i, stk.peek());
+            }
+            stk.push(arr[i]);
+            i--;
+        }
+        
+        i = n-1;
+        while(i > ind) {
+            while(!stk.isEmpty() && stk.peek() <= arr[i]) stk.pop();
+            int temp = arr[i];
+            if(!stk.isEmpty()) {
+                ll.set(i, stk.peek());
+            }
+            stk.push(arr[i]);
+            i--;
+        }
+        return ll;
+    }
+}
